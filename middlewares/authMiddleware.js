b69575cf47407902c080d2d8d0a00dc45c6cc94c
Arvanit Telaku <arvanit.telaku05@gmail.com>
2025-07-18 06:51:17 +0200

    Auth middleware modified with allow roles method added!

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,22 +1,37 @@
 const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+const { UnauthorizedError, ForbiddenError } = require('../utils/appError');
 
-const authenticate = (req,res,next) => {
+//middleware to verify jwt and attach user to the request
+const authenticate = async (req, res, next) => {
     const authHeader = req.headers.authorization;
 
-    //if token is present
-    if(!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json(({message: 'Unauthorized: No token provided!'}));
+    if(!authHeader?.startsWith('Bearer ')) {
+        return next(new UnauthorizedError('Token missing or invalid!'));
     }
 
     const token = authHeader.split(' ')[1];
 
-    try{
+    try {
         const decoded = jwt.verify(token,process.env.JWT_SECRET);
-        req.user = decoded;
+        const user = await User.findByPk(decoded.id);
+        if(!user) return next(new UnauthorizedError('User not found!'));
+        req.user = user;
         next();
-    }catch(err) {
-        return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+    }catch(err){
+        next(new UnauthorizedError('Invalid or expired token!'));
     }
 };
 
-module.exports = authenticate;  
+
+//middleware to allow roles 
+const allowRoles = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)) {
+            return next(new ForbiddenError('You are not authorized!'))
+        }
+        next();
+    };
+};
+
+module.exports = {authenticate,allowRoles}; 
