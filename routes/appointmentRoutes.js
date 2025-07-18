e4f4a39bce7066fe41f8b2c6f023bbfa0a886a47
Arvanit Telaku <arvanit.telaku05@gmail.com>
2025-07-18 06:55:35 +0200

    Appointment routes updated with middlewares!

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const appointmentController = require('../controllers/appointment.controller');
 const authenticate = require('../middlewares/auth.middleware');
+const { allowRoles } = require('../middlewares/authMiddleware');
 
-router.post('/', authenticate, appointmentController.createAppointment);
-router.get('/', authenticate, appointmentController.getAppointments);
+router.post('/', authenticate,allowRoles('admin,receptionist') ,appointmentController.createAppointment);
+router.get('/', authenticate,allowRoles('admin','receptionist'), appointmentController.getAppointments);
 router.get('/:id', authenticate, appointmentController.getAppointmentById);
-router.put('/:id', authenticate, appointmentController.updateAppointment);
-router.delete('/:id', authenticate, appointmentController.deleteAppointment);
+router.put('/:id', authenticate,allowRoles('admin','receptionist'), appointmentController.updateAppointment);
+router.delete('/:id', authenticate,allowRoles('admin','receptionist'), appointmentController.deleteAppointment);
 
 module.exports = router;
