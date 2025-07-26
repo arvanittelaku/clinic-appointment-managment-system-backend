7fdc07b631a6deab28e830acfdaacc605c802069
Arvanit Telaku <arvanit.telaku05@gmail.com>
2025-07-27 00:59:10 +0200

    appointment routes updated with appointment validation.

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
+
 const appointmentController = require('../controllers/appointmentController');
 const { authenticate } = require('../middlewares/authMiddleware');
-const authorize = require('../middlewares/authorize'); 
-const { createAppointmentValidation, updateAppointmentValidation } = require('../validations/appointmentValidation');
+const authorize = require('../middlewares/authorize');
+
+const {
+  createAppointmentValidation,
+  updateAppointmentValidation,
+} = require('../validations/appointmentValidation'); 
+
 const validate = require('../middlewares/validate');
-
 router.use(authenticate);
-
 router.get('/available-slots', appointmentController.getAvailableSlots);
-
 router.post(
   '/',
   createAppointmentValidation,
@@ -17,9 +20,9 @@ router.post(
   appointmentController.createAppointment
 );
 
+
 router.get('/', appointmentController.getAppointments);
 router.get('/:id', appointmentController.getAppointmentById);
-
 router.patch(
   '/:id',
   updateAppointmentValidation,
